4a6d65c4fda5e9b2a6546d5aa2edb094c3608b0b
Sneider Narvaez <sneider.developer@gmail.com>
2026-07-01 00:18:44 -0500

    Refactor DropdownMenuField to use StatelessWidget and simplify state management

--- widgets/dropdown_menu_field.dart
+++ widgets/dropdown_menu_field.dart
@@ -1,14 +1,14 @@
 
 import 'package:flutter/material.dart';
 
-class DropdownMenuField extends StatefulWidget {
+class DropdownMenuField extends StatelessWidget {
   const DropdownMenuField({
     super.key,
     this.label,
     this.value,
     required this.items,
     required this.onChanged,
-    this.width
+    this.width,
   });
 
   final String? label;
@@ -18,42 +18,29 @@ class DropdownMenuField extends StatefulWidget {
   final double? width;
 
   @override
-  State<DropdownMenuField> createState() => _DropdownMenuFieldState();
-}
-
-class _DropdownMenuFieldState extends State<DropdownMenuField> {
-  final controller = TextEditingController();
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return DropdownMenuFormField<String>(
-      label: Text(widget.label ?? ''),
-      controller: controller,
-      onSelected: widget.onChanged,
-      dropdownMenuEntries: widget.items,
-      textStyle: TextStyle(
+      label: Text(label ?? ''),
+      initialSelection: value,
+      onSelected: onChanged,
+      dropdownMenuEntries: items,
+      textStyle: const TextStyle(
         color: Color(0xFF162F48),
       ),
-      width: widget.width,
+      width: width,
       inputDecorationTheme: InputDecorationTheme(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(14),
-          borderSide: BorderSide(color: Color(0xFFC6D2DE))
+          borderSide: const BorderSide(color: Color(0xFFC6D2DE)),
         ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(14),
-          borderSide: BorderSide(color: Color(0xFFC6D2DE))
+          borderSide: const BorderSide(color: Color(0xFFC6D2DE)),
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(14),
-          borderSide: BorderSide(color: Color(0xFFC6D2DE), width: 1.5)
-        )
+          borderSide: const BorderSide(color: Color(0xFFC6D2DE), width: 1.5),
+        ),
       ),
     );
   }
