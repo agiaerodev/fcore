b340ea4c771512791c40ab96fb9d39224f0a87da
Sneider Narvaez <sneider.developer@gmail.com>
2026-07-14 12:25:30 -0500

    Add fa_icon_map.g.dart to define Font Awesome icon mappings

---
+++ utils/fa_icon_map.g.dart
@@ -0,0 +1,26 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const Map<String, FaIconData> faSolidIcons = {
+  'bell-concierge': FontAwesomeIcons.bellConcierge,
+  'bell-slash': FontAwesomeIcons.solidBellSlash,
+  'capricorn': FontAwesomeIcons.capricorn,
+  'capsules': FontAwesomeIcons.capsules,
+  'car': FontAwesomeIcons.car,
+  'zap': FontAwesomeIcons.bolt,
+};
+
+const Map<String, FaIconData> faRegularIcons = {
+  'address-book': FontAwesomeIcons.addressBook,
+  'address-card': FontAwesomeIcons.addressCard,
+  'calendar-times': FontAwesomeIcons.calendarXmark,
+  'window-restore': FontAwesomeIcons.windowRestore,
+  'xmark-circle': FontAwesomeIcons.circleXmark,
+};
+
+const Map<String, FaIconData> faBrandIcons = {
+  'behance': FontAwesomeIcons.behance,
+  'behance-square': FontAwesomeIcons.squareBehance,
+  'cc-mastercard': FontAwesomeIcons.ccMastercard,
+  'expeditedssl': FontAwesomeIcons.expeditedssl,
+  'facebook': FontAwesomeIcons.facebook,
+};
